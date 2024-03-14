aac9a66cc1688b090fe5fb4bcbf055a1683fe053
Kirill <sosisks888@mail.ru>
2024-03-14 15:14:19 +0300

    add crud services

--- DynamicSunTestTask/Data/ApplicationContext.cs
+++ DynamicSunTestTask/Data/ApplicationContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DynamicSunTestTask.Entites;
+using Microsoft.EntityFrameworkCore;
 
 namespace DynamicSunTestTask.Data
 {
 	public class ApplicationContext : DbContext
 	{
+		public virtual DbSet<WheatherColumnEntity> WheatherColumnsEntitis => Set<WheatherColumnEntity>();
+
 		public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
 		{ Database.EnsureCreated(); }
 
