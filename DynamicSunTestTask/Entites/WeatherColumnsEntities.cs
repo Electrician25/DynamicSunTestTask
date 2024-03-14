f86c85fe4a8887364929ed4b65dc2d64f49e53fb
Kirill <sosisks888@mail.ru>
2024-03-14 14:39:52 +0300

    add entities

---
+++ DynamicSunTestTask/Entites/WeatherColumnsEntities.cs
@@ -0,0 +1,18 @@
+﻿namespace DynamicSunTestTask.Entites
+{
+	public class WeatherColumnsEntities
+	{
+		public int Date { get; set; }
+		public string MoskowTime { get; set; } = null!;
+		public double Temperature { get; set; }
+		public double AirHumidity { get; set; }
+		public double DewPoint { get; set; }
+		public int Pressure { get; set; }
+		public string? DirectionOfTheWind { get; set; }
+		public string? WindSpeed { get; set; }
+		public int Cloudy { get; set; }
+		public int lowerCloudLimit { get; set; }
+		public int HorizontalVisibility { get; set; }
+		public string? WeatherConditions { get; set; }
+	}
+}
