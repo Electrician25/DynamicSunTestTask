17a729595d24b7f55849e6e789d407fa99deafa3
Kirill <sosisks888@mail.ru>
2024-03-14 14:13:19 +0300

    Add project files.

---
+++ DynamicSunTestTask/Data/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DynamicSunTestTask.Data
+{
+	public class ApplicationContext : DbContext
+	{
+		public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+		{ Database.EnsureCreated(); }
+
+		protected override void OnModelCreating(ModelBuilder modelbuilder)
+		{ }
+	}
+}
