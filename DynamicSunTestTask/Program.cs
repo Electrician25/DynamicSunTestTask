0a8bd551efc5398babace2ea767d3bfdec2712f9
Kirill <sosisks888@mail.ru>
2024-03-14 22:15:44 +0300

    add html controller and html main page

--- DynamicSunTestTask/Program.cs
+++ DynamicSunTestTask/Program.cs
@@ -1,14 +1,32 @@
+using DynamicSunTestTask.ActionResult;
 using DynamicSunTestTask.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddRouting();
+
 builder.AddApplicationContext();
+
 builder.Services.AddControllers();
+
 builder.Services.AddCategoryCrudServices();
+
+builder.Services.AddTransient(provider =>
+{
+	return new Func<string, HtmlResult>(
+		path => ActivatorUtilities.CreateInstance<HtmlResult>(provider, path));
+});
+
 var app = builder.Build();
+
 app.UseRouting();
+
 app.MapControllers();
+
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
+
 app.UseRouting();
+
 app.Run();
